e5e7e835183bc024256ba91bc60c07c672ad1141
sly <sylvester@archis.company>
2023-06-24 08:21:01 +0100

    update: backup

--- lvim/.config/lvim/lua/user/autocmd.lua
+++ lvim/.config/lvim/lua/user/autocmd.lua
@@ -25,7 +25,10 @@ lvim.autocommands = {
     {
       pattern = "*",
       callback = function()
-        vim.api.nvim_set_hl(0, "VertSplit", { bg = "#1E2327", fg = "#303436", underline = false, bold = true })
+        vim.api.nvim_set_hl(0, "VertSplit", { bg = "#1E2327", fg = "#272C2F", underline = false, bold = true })
+        vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "#1E2327", fg = "#303436", bold = true })
+        vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "#1E2327", fg = "#303436", bold = true })
+        vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "#1E2327", fg = "#303436", bold = true })
       end,
     },
   },
