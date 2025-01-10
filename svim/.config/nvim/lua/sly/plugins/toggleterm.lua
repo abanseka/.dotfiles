ee0ccc829535edfc31bba0840b94fc8f83bf488f
sly <sylvester@archis.company>
2025-01-10 08:26:52 +0100

    🍿 Backup

--- svim/.config/nvim/lua/sly/plugins/toggleterm.lua
+++ svim/.config/nvim/lua/sly/plugins/toggleterm.lua
@@ -22,13 +22,29 @@ return {
 			},
 		})
 
+		local lazydocker = Terminal:new({
+			cmd = "lazydocker",
+			hidden = true,
+			direction = "float",
+			float_opts = {
+				border = "",
+				width = use_window_size,
+				height = use_window_size,
+			},
+		})
+
 		function lazygit_toggle()
 			lazygit:toggle()
+		end
+
+		function lazydocker_toggle()
+			lazydocker:toggle()
 		end
 
 		vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>exe v:count1 . 'ToggleTerm'<CR>", opt)
 		vim.api.nvim_set_keymap("n", "<leader>ta", "<cmd>ToggleTermToggleAll<CR>", opt)
 		vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua lazygit_toggle()<CR>", opt)
+		vim.api.nvim_set_keymap("n", "<leader>dd", "<cmd>lua lazydocker_toggle()<CR>", opt)
 
 		---@diagnostic disable-next-line: duplicate-set-field
 		function _G.set_terminal_keymaps()
